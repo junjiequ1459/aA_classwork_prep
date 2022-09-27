6c9dc25072ae1a49ec1d3177d14c3677b4d07daa
RexQu <junejiequ123@gmail.com>
2022-09-27 19:43:28 -0400

    W1D4 problemset

--- ProblemSet/W1D4problemSet.js
+++ ProblemSet/W1D4problemSet.js
@@ -44,9 +44,49 @@ const wordPeriods = (sentence) => {
   let newSentence = sentence.split(" ");
   let returnSentence = "";
   for (let i = 0; i < newSentence.length; i++) {
+    // [hello,world]
     returnSentence += `${newSentence[i]}. `;
   }
   console.log(returnSentence);
 };
 wordPeriods("hello world"); // => 'hello. world.'
 wordPeriods("what is the weather today"); // => 'what. is. the. weather. today.'
+
+const maxValue = (array) => {
+  returnValue = null;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > returnValue) {
+      returnValue = array[i];
+    }
+  }
+  console.log(returnValue);
+};
+
+maxValue([12, 6, 43, 2]); // => 43
+maxValue([]); // => null
+maxValue([-4, -10, 0.43]); // => 0.43
+
+const myIndexOf = (array, target) => {
+  for (let i = 0; i < array.length; i++) {
+    if (target === array[i]) {
+      return console.log(i);
+    }
+  }
+  return console.log(-1);
+};
+
+myIndexOf([1, 2, 3, 4], 4); //=> 3
+myIndexOf([5, 6, 7, 8], 2); //=> -1
+
+const factorArray = (array, number) => {
+  returnArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (number % array[i] === 0) {
+      returnArr.push(array[i]);
+    }
+  }
+  console.log(returnArr);
+};
+factorArray([2, 3, 4, 5, 6], 20); //=> [2,4,5]
+factorArray([2, 3, 4, 5, 6], 35); //=> [5]
+factorArray([10, 15, 20, 25], 5); //=> []
